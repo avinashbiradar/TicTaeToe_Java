57d1d1bd293d06a01f2e128a09f069b65ede4f21
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-21 08:36:11 +0530

    Added:Method to create board

--- TicTaeToeGame.java
+++ TicTaeToeGame.java
@@ -2,6 +2,14 @@ public class TicTaeToeGame {
     public  static void main(String[]args)
     {
         System.out.println("Welcome to tic-tae-toe game");
+        char[] board= createBoard();
     }
-
+    //uc1 - create empty 10 size char array for board
+    private static char[] createBoard() {
+        char[] board = new char[10];
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        return board;
+    }
 }
