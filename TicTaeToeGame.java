88548afc47a9bc596900c1f3894cb3dfe70da423
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-21 08:56:09 +0530

    Added:method to choose letter for user

--- TicTaeToeGame.java
+++ TicTaeToeGame.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class TicTaeToeGame {
     public  static void main(String[]args)
     {
         System.out.println("Welcome to tic-tae-toe game");
         char[] board= createBoard();
+        Scanner userInput = new Scanner(System.in);
+        char userLetter = chooseUserLetter(userInput);
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
+
     }
     //uc1 - create empty 10 size char array for board
     private static char[] createBoard() {
@@ -11,5 +17,11 @@ public class TicTaeToeGame {
             board[i] = ' ';
         }
         return board;
+    }
+    //uc2 choose player letter X or O
+    private static char chooseUserLetter(Scanner userInput){
+        System.out.println("Choose your letter");
+        return userInput.next().toUpperCase().charAt(0);
+
     }
 }
